f9973f2d03ab97435c76b6201f4c36673c32a437
aditya-gupta-123 <99164802+aditya-gupta-123@users.noreply.github.com>
2026-09-25 22:28:54 +0530

    Enhance BinaryHeap with method descriptions
    Added comments to describe methods and their functionality.

--- heap.js
+++ heap.js
@@ -1,22 +1,29 @@
 export class BinaryHeap {
 
     heap = [];
-
+    
+    // inserts a new value at the end of the heap and bubbles it up to its correct position
     insert(value) {
         console.log(value);
         this.heap.push(value);
         this.bubbleUp();
     }
 
+    // Returns the total number of items currently in the heap.
     size() {
         return this.heap.length;
     }
 
+    // Returns true if the heap is empty, false otherwise.
     empty(){
         return ( this.size()===0 );
     }
 
     //using iterative approach
+    //1. Starts at the newly added item (index = this.size() - 1).
+    //2. Calculates the parent's position using Math.floor((index - 1) / 2).
+    //3. Compares element[0] against parent[0].
+    //4. If the new element is larger than its parent, it swaps them and continues moving up the tree until the parent is greater or it reaches the root (index = 0).
     bubbleUp() {
         let index = this.size() - 1;
 
@@ -32,6 +39,7 @@ export class BinaryHeap {
         }
     }
 
+    // Removes and returns the maximum value from the heap, which is always at index 0. It replaces the root with the last element in the heap and then sinks it down to its correct position.
     extractMax() {
         const max = this.heap[0];
         const tmp = this.heap.pop();
@@ -42,6 +50,11 @@ export class BinaryHeap {
         return max;
     }
 
+    //using recursive approach
+    //1. Starts at the root (index = 0).
+    //2. Calculates the left and right children's positions using 2 * index + 1 and 2 * index + 2, respectively.
+    //3. Compares the current element with its children and swaps it with the larger child if necessary.
+    //4. Continues this process recursively until the current element is larger than both children or it reaches a leaf node.
     sinkDown(index) {
 
         let left = 2 * index + 1,
